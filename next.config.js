9559487e3a86e5071181d3b7038db4c3ef101e9d
Diego Hernandez <diegohxxiv@gmail.com>
2023-11-01 23:35:45 -0700

    v1 done

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ["raw.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
   },
 };
 
